44810733cbf3a3d6aa9c892e5fda277b574b73a2
mahesh-paliwal <maheshpaliwalwps@gmail.com>
2026-08-23 23:44:36 +0530

    Add new column uuid in staffparmissions table

--- app/Models/StaffPermission.php
+++ app/Models/StaffPermission.php
@@ -4,12 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class StaffPermission extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['staff_id', 'permission_key'];
+    protected $fillable = ['uuid', 'staff_id', 'permission_key'];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            $model->uuid = (string) Str::uuid();
+        });
+    }
 
     public function staff()
     {

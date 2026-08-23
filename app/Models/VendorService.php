6103e11fb0871a6f4e6e578ffd029bced4061035
mahesh-paliwal <maheshpaliwalwps@gmail.com>
2026-08-24 00:26:00 +0530

    Add new column uuid in vendorservices 2 table

--- app/Models/VendorService.php
+++ app/Models/VendorService.php
@@ -4,12 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class VendorService extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['vendor_id', 'service_id', 'price_per_unit', 'is_active'];
+    protected $fillable = ['uuid', 'vendor_id', 'service_id', 'price_per_unit', 'is_active'];
+
+    protected static function booted()
+    {
+        static::creating(function ($vendorService) {
+            $vendorService->uuid = (string) Str::uuid();
+        });
+    }
 
     protected function casts(): array
     {

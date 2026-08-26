6557a20ff6f5b3919c357153b9a0d64068793c96
mahesh-paliwal <maheshpaliwalwps@gmail.com>
2026-08-26 11:29:44 +0530

    Add column in uders table

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,6 +19,8 @@ class User extends Authenticatable
         'password',
         'role_id',
         'uuid',
+        'address',
+        'status',
     ];
 
     protected $hidden = [
@@ -38,6 +40,7 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'status' => 'boolean',
         ];
     }
 
